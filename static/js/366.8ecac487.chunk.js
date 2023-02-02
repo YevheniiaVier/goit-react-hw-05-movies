fd3fabb422f06a2a98ad864ebcd79efb91a89a17
YevheniiaVier <107929873+YevheniiaVier@users.noreply.github.com>
2023-02-02 14:18:53 +0000

    Deploying to gh-pages from @ YevheniiaVier/goit-react-hw-05-movies@e75d700d85570e0140a61133763b8d6d8d2e3656 🚀

---
+++ static/js/366.8ecac487.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[366],{9366:function(e,t,r){r.r(t);var n=r(6681),a=r(184);t.default=function(){return(0,a.jsxs)(n.xu,{children:[(0,a.jsx)(n.Dx,{children:"Not Existing Page"}),(0,a.jsx)(n.Fg,{to:"/",children:"To Home page"})]})}}}]);
+//# sourceMappingURL=366.8ecac487.chunk.js.map
